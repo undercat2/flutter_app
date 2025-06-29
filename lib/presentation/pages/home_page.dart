1ab1865ec564de768f13e7e402432158db2f921c
undercat2 <under_cat2@web.de>
2025-06-29 15:49:03 +0200

    Update home_page.dart with full implementation

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,1 +1,79 @@
-// Home page
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/home/home_bloc.dart';
+import '../bloc/home/home_event.dart';
+import '../bloc/home/home_state.dart';
+import '../widgets/item_card.dart';
+import 'item_detail_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<HomeBloc>().add(HomeItemsRequested());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[50],
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'Smart Home',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ),
+            Expanded(
+              child: BlocBuilder<HomeBloc, HomeState>(
+                builder: (context, state) {
+                  switch (state.status) {
+                    case HomeStatus.loading:
+                      return Center(child: CircularProgressIndicator());
+                    case HomeStatus.success:
+                      return ListView.builder(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        itemCount: state.items.length,
+                        itemBuilder: (context, index) {
+                          final item = state.items[index];
+                          return ItemCard(
+                            item: item,
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (_) => ItemDetailPage(item: item),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      );
+                    case HomeStatus.failure:
+                      return Center(
+                        child: Text('Failed to load items'),
+                      );
+                    default:
+                      return Container();
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
